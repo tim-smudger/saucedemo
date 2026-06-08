2e2e97e35cea2d4ef3a7e7c6a05b8852b96f5fd1
Tim <tcs_one@hotmail.com>
2026-06-08 22:23:59 +0100

    adding screenshots on failure and putting repprts on github pages

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
 
     /* Run headless in CI, headed locally */
     headless: !!process.env.CI,
